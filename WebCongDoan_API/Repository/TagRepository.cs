6df265cc565f45c8af5c92cc983311236f855188
sSyRuSs <89365131+sSyRuSs@users.noreply.github.com>
2023-05-27 23:22:09 +0700

    AddTagController

---
+++ WebCongDoan_API/Repository/TagRepository.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebCongDoan_API.Interfaces;
+using WebCongDoan_API.Models;
+using WebCongDoan_API.ViewModels;
+
+namespace WebCongDoan_API.Repository
+{
+    public class TagRepository:ITagRepository
+    {
+        private readonly MyDBContext _context;
+        private readonly IMapper _mapper;
+
+        public TagRepository(MyDBContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task AddTag(TagVM tagVM)
+        {
+            var tag = _mapper.Map<Tag>(tagVM);
+            _context.Tags.Add(tag);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteTag(int id)
+        {
+            var tag = _context.Tags.SingleOrDefault(p => p.TagId == id);
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<TagVM>> GetAllTags()
+        {
+            var tag = await _context.Tags.ToListAsync();
+            return _mapper.Map<List<TagVM>>(tag);
+        }
+
+        public async Task<TagVM> GetTagById(int id)
+        {
+            var tag = await _context.Tags.FindAsync(id);
+            return _mapper.Map<TagVM>(tag);
+        }
+
+        public async Task UpdateTag(TagVM tagVM)
+        {
+            var tag = _mapper.Map<Tag>(tagVM);
+            _context.Tags.Update(tag);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
