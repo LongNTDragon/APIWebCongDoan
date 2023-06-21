9ed8161f811a0615704c01bbdf957ca51dd57387
sSyRuSs <89365131+sSyRuSs@users.noreply.github.com>
2023-06-21 17:17:30 +0700

    delete comID

---
+++ WebCongDoan_API/Migrations/20230621101017_delete_ComId_tblQuestion.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCongDoan_API.Migrations
+{
+    public partial class delete_ComId_tblQuestion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ComId",
+                table: "Questions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ComId",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
